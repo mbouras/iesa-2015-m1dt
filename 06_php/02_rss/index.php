f1688fbb6fcea41ad7b10bf5cce46b5eebb8b37a
mbouras <marine.bouras@gmail.com>
2015-03-19 14:31:53 +0100

    TP PHP : créer 2 tableaux de 2 manières différentes qui contiennent chacun une news d'un flux rss

--- 06_php/02_rss/index.php
+++ 06_php/02_rss/index.php
@@ -1,1 +1,28 @@
-index.php
+<?php
+
+$news1 = array(
+    'title' => 'Que diriez-vous d’explorer le centre de la Terre depuis votre navigateur ?',
+    'link' => 'http://www.presse-citron.net/que-diriez-vous-dexplorer-le-centre-de-la-terre-depuis-votre-navigateur/',
+    'pubDate' => 'Thu, 19 Mar 2015 08:41:20 +0000',
+    'description' => 'La BBC vous invite à prendre votre casque et votre marteau piqueur, pour vous lancer dans une belle épopée virtuelle et stylisée.'
+);;
+// $newTwo = array(
+//     'title' => 'Microsoft annonce la mort d’Internet Explorer',
+//     'link' => 'http://www.presse-citron.net/microsoft-annonce-la-mort-dinternet-explorer-son-celebre-navigateur/',
+//     'pubDate' => 'Wed, 18 Mar 2015 22:30:30 +0000',
+//     'description' => 'Internet Explorer, le navigateur qui fut roi pendant des années devrait disparaître à petit feu et être remplacé par un nouveau navigateur.'
+// );;
+$news2['title'] = 'Microsoft annonce la mort d’Internet Explorer';
+$news2['link'] = 'http://www.presse-citron.net/microsoft-annonce-la-mort-dinternet-explorer-son-celebre-navigateur/';
+$news2['pubDate'] = 'Wed, 18 Mar 2015 22:30:30 +0000';
+$news2['description'] = 'Internet Explorer, le navigateur qui fut roi pendant des années devrait disparaître à petit feu et être remplacé par un nouveau navigateur.';
+
+
+$news = array(
+    'firstNews' => $news1,
+    'secondNews' => $news2 
+);
+
+var_dump($news);;
+
+?>
