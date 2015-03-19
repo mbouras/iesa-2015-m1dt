135fc233a5a3de47b6d3fdb15c92e87f8ec6af82
mbouras <marine.bouras@gmail.com>
2015-03-19 12:37:52 +0100

    TP PHP : modifier un code qui n'était pas bon/propre en terme de conception

--- 06_php/index.php
+++ 06_php/index.php
@@ -1,24 +1,57 @@
 <?php 
+    //--AFFICHER LES INFOS PHP DE LA PAGE
     //phpinfo();
 
-    // $a = 1;
 
+    //--TESTER LES GLOBALES
+    // $a = 1;
     // function test(){
     //     global $a;
     //     echo $a;
     // }
-
     // test();
 
-    $a = "bonjour";
-    $$a = "monde";
+        //--Code pas bon
+        $a = 1;
+        $b = 2;
+        // function somme(){
+        //     global $a, $b;
+        //     $b = $a + $b;
+        // }
+        // somme();
+        // echo $b;
 
-    echo $a."<br />";
-    echo $$a."<br />";
-    echo $bonjour."<br />";
+        //--Code bon
+        function somme($c, $d){
+            return $c + $d;
+        }
+        echo somme($a, $b);
 
 
+    //--VARIABLES DYNAMIQUES
+    // $a = "bonjour";
+    // $$a = "monde";
+    // echo $a."<br />";
+    // echo $$a."<br />";
+    // echo $bonjour."<br />";
+    // var_dump($a);
+    echo("<br />");
+    
+
+    //--CONSTANTES
     //define("AIRPLANE", "D112");
     const AIRPLANE = "D112";
-    echo AIRPLANE;
+    echo AIRPLANE."<br />";
+
+
+    //--TABLEAU
+    $monTableau = array();
+    var_dump($monTableau);
+    echo("<br />");
+    $course = array(
+        "fruits" => "pomme",
+        "magasin" => "Auchan",
+        "prix" => "1€"
+    );
+    var_dump($course);
 ?>
