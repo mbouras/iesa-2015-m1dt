07a7a32b0766d2a4f74541a3baf0a00c0c9bba3b
mbouras <marine.bouras@gmail.com>
2015-03-19 15:46:16 +0100

    TP PHP : correction bug affichage -> ajout balise fermante <table>

--- 06_php/02_rss/index.php
+++ 06_php/02_rss/index.php
@@ -1,12 +1,12 @@
 <?php
 
 $news1 = array(
-    'title' => 'Que diriez-vous d’explorer le centre de la Terre depuis votre navigateur ?',
+    'title' => 'NEWS01--Que diriez-vous d’explorer le centre de la Terre depuis votre navigateur ?',
     'link' => 'http://www.presse-citron.net/que-diriez-vous-dexplorer-le-centre-de-la-terre-depuis-votre-navigateur/',
     'pubDate' => 'Thu, 19 Mar 2015 08:41:20 +0000',
     'description' => 'La BBC vous invite à prendre votre casque et votre marteau piqueur, pour vous lancer dans une belle épopée virtuelle et stylisée.'
 );;
-$news2['title'] = 'Microsoft annonce la mort d’Internet Explorer';
+$news2['title'] = 'NEWS002--Microsoft annonce la mort d’Internet Explorer';
 $news2['link'] = 'http://www.presse-citron.net/microsoft-annonce-la-mort-dinternet-explorer-son-celebre-navigateur/';
 $news2['pubDate'] = 'Wed, 18 Mar 2015 22:30:30 +0000';
 $news2['description'] = 'Internet Explorer, le navigateur qui fut roi pendant des années devrait disparaître à petit feu et être remplacé par un nouveau navigateur.';
@@ -42,7 +42,8 @@ function buildTableForNews($table){
                 <td>'.$table['link'].'</td>
                 <td>'.$table['pubDate'].'</td>
                 <td>'.$table['description'].'</td>
-            </tr>';
+            </tr>
+        </table>';
 }
 function buildHtmlRowFor($news){
     $line = '<ul>';
@@ -65,9 +66,16 @@ function displayRow($news){
         <title>TP PHP : Tableaux</title>
     </head>
     <body>
+        <h2>News avec une liste</h2>
         <?php 
             echo buildHtmlForNews($news1);
-            //echo buildTableForNews($news2);
+        ?>
+        <h2>News avec un tableau</h2>
+        <?php 
+            echo buildTableForNews($news2);
+        ?>
+        <h2>News avec un foreach</h2>
+        <?php
             echo displayRow($news1);
         ?>
     </body>
