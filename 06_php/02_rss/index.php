0e8dc896743316514bc0e43482f9c1a43b53cc13
mbouras <marine.bouras@gmail.com>
2015-03-19 14:48:08 +0100

    TP PHP : création d'une fonction pour afficher une news sous forme de liste HTML

--- 06_php/02_rss/index.php
+++ 06_php/02_rss/index.php
@@ -6,23 +6,42 @@ $news1 = array(
     'pubDate' => 'Thu, 19 Mar 2015 08:41:20 +0000',
     'description' => 'La BBC vous invite à prendre votre casque et votre marteau piqueur, pour vous lancer dans une belle épopée virtuelle et stylisée.'
 );;
-// $newTwo = array(
-//     'title' => 'Microsoft annonce la mort d’Internet Explorer',
-//     'link' => 'http://www.presse-citron.net/microsoft-annonce-la-mort-dinternet-explorer-son-celebre-navigateur/',
-//     'pubDate' => 'Wed, 18 Mar 2015 22:30:30 +0000',
-//     'description' => 'Internet Explorer, le navigateur qui fut roi pendant des années devrait disparaître à petit feu et être remplacé par un nouveau navigateur.'
-// );;
 $news2['title'] = 'Microsoft annonce la mort d’Internet Explorer';
 $news2['link'] = 'http://www.presse-citron.net/microsoft-annonce-la-mort-dinternet-explorer-son-celebre-navigateur/';
 $news2['pubDate'] = 'Wed, 18 Mar 2015 22:30:30 +0000';
 $news2['description'] = 'Internet Explorer, le navigateur qui fut roi pendant des années devrait disparaître à petit feu et être remplacé par un nouveau navigateur.';
 
 
-$news = array(
-    'firstNews' => $news1,
-    'secondNews' => $news2 
-);
+// $news = array(
+//     'firstNews' => $news1,
+//     'secondNews' => $news2 
+// );
 
-var_dump($news);;
+//var_dump($news);;
+
+function buildHtmlForNews($table){
+    return
+        '<ul>
+            <li>'.$table['title'].'</li>'.
+            '<li>'.$table['link'].'</li>'.
+            '<li>'.$table['pubDate'].'</li>'.
+            '<li>'.$table['description'].'</li>'.
+        '</ul>';
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>TP PHP : Tableaux</title>
+    </head>
+    <body>
+        <?php 
+            echo buildHtmlForNews($news1);
+            echo buildHtmlForNews($news2);
+        ?>
+    </body>
+</html>
+
